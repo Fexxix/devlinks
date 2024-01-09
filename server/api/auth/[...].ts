5adb147fd712aae833787322253e46dd3dc1cad0
fexxix <fexxix@gmail.com>
2024-01-09 12:37:01 +0500

    style: styled the login page

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -4,7 +4,7 @@ import GithubProvider from "next-auth/providers/github"
 export default NuxtAuthHandler({
   secret: process.env.AUTH_SECRET,
   pages: {
-    signIn: "/signin",
+    signIn: "/login",
   },
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
